8fa0c2492699db51e95560ec0adc839f09b853f0
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2020-02-18 19:09:15 +0700

    Validating HTTP POST Request Body

--- mobile-app-ws/src/main/java/com/bk/blog/app/ws/ui/model/request/UserDetailsRequestModel.java
+++ mobile-app-ws/src/main/java/com/bk/blog/app/ws/ui/model/request/UserDetailsRequestModel.java
@@ -1,9 +1,25 @@
 package com.bk.blog.app.ws.ui.model.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UserDetailsRequestModel {
+
+    @NotNull(message = "first name can not be null")
+    @Size(min = 2, message = "The first name must be grater than 2 characters")
     private String firstName;
+
+    @NotNull(message = "last name can not be null")
+    @Size(min = 2, message = "The last name must be grater than 2 characters")
     private String lastName;
+
+    @NotNull(message = "email can not be null")
+    @Email
     private String email;
+
+    @NotNull(message = "password can not be null")
+    @Size(min=8, max=16, message = "Password must be equal or grater than 8 and less than 16 characters")
     private String password;
 
     public String getFirstName() {
