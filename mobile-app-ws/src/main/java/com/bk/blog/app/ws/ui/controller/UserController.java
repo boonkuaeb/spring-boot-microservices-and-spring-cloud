651133a0029f9b9de3374e7da5a9ece3f47a4752
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2020-02-18 18:33:56 +0700

    Returning Java Object as Return Value

--- mobile-app-ws/src/main/java/com/bk/blog/app/ws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/bk/blog/app/ws/ui/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.bk.blog.app.ws.ui.controller;
 
+import com.bk.blog.app.ws.ui.model.response.UserRest;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,17 +9,22 @@ public class UserController {
 
     @GetMapping
     public String getUsers(
-            @RequestParam( value = "page") int page,
-            @RequestParam( value = "limit") int limit
+            @RequestParam( value = "page" , defaultValue = "1") int page,
+            @RequestParam( value = "limit", defaultValue = "50") int limit,
+            @RequestParam( value = "sort", defaultValue = "desc", required = false) String sort
     )
     {
-        return "get user all was called with page = "+page + " and limit =" +limit;
+        return "get user all was called with page = "+page + " and limit =" +limit + " and sort = "+sort;
     }
 
-    @GetMapping(path = "/userId")
-    public String getUser(@PathVariable String userId)
+    @GetMapping(path = "/{userId}")
+    public UserRest getUser(@PathVariable String userId)
     {
-        return "get user was called with user ID = "+userId;
+        UserRest returnValue = new UserRest();
+        returnValue.setEmail("test@mail.com");
+        returnValue.setFirstName("test_firstname");
+        returnValue.setLastName("test_lastname");
+        return  returnValue;
     }
 
     @PostMapping
