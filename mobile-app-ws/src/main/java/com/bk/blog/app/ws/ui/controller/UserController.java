2ab6143a1149aca3ac6ee9b3759efe25e0af2ff0
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2020-02-18 18:20:53 +0700

    Reading Query String Request

---
+++ mobile-app-ws/src/main/java/com/bk/blog/app/ws/ui/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.bk.blog.app.ws.ui.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+    @GetMapping
+    public String getUsers(
+            @RequestParam( value = "page") int page,
+            @RequestParam( value = "limit") int limit
+    )
+    {
+        return "get user all was called with page = "+page + " and limit =" +limit;
+    }
+
+    @GetMapping(path = "/userId")
+    public String getUser(@PathVariable String userId)
+    {
+        return "get user was called with user ID = "+userId;
+    }
+
+    @PostMapping
+    public String createUser()
+    {
+        return "create user was called";
+    }
+
+    @PutMapping
+    public String updateUser()
+    {
+        return "update user was called";
+    }
+    @DeleteMapping
+    public String deleteUser()
+    {
+        return "delete user was called";
+    }
+}
