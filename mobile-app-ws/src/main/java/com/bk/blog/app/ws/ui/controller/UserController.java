d658359660e87a89a7579b7a10846378a0ce0e04
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2020-02-18 18:43:39 +0700

    Returning Object as JSON or XML Representation

--- mobile-app-ws/src/main/java/com/bk/blog/app/ws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/bk/blog/app/ws/ui/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.bk.blog.app.ws.ui.controller;
 
 import com.bk.blog.app.ws.ui.model.response.UserRest;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,38 +10,39 @@ public class UserController {
 
     @GetMapping
     public String getUsers(
-            @RequestParam( value = "page" , defaultValue = "1") int page,
-            @RequestParam( value = "limit", defaultValue = "50") int limit,
-            @RequestParam( value = "sort", defaultValue = "desc", required = false) String sort
-    )
-    {
-        return "get user all was called with page = "+page + " and limit =" +limit + " and sort = "+sort;
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "limit", defaultValue = "50") int limit,
+            @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort
+    ) {
+        return "get user all was called with page = " + page + " and limit =" + limit + " and sort = " + sort;
     }
 
-    @GetMapping(path = "/{userId}")
-    public UserRest getUser(@PathVariable String userId)
-    {
+    @GetMapping(path = "/{userId}",
+            produces = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE
+            }
+    )
+    public UserRest getUser(@PathVariable String userId) {
         UserRest returnValue = new UserRest();
         returnValue.setEmail("test@mail.com");
         returnValue.setFirstName("test_firstname");
         returnValue.setLastName("test_lastname");
-        return  returnValue;
+        return returnValue;
     }
 
     @PostMapping
-    public String createUser()
-    {
+    public String createUser() {
         return "create user was called";
     }
 
     @PutMapping
-    public String updateUser()
-    {
+    public String updateUser() {
         return "update user was called";
     }
+
     @DeleteMapping
-    public String deleteUser()
-    {
+    public String deleteUser() {
         return "delete user was called";
     }
 }
